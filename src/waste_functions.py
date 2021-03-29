32f5d11c4ab9c1898954fcb70483621370159b85
L.Zolyomi <zolyomilevi@gmail.com>
2021-03-30 00:07:57 +0200

    merged waste functions

--- src/waste_functions.py
+++ src/waste_functions.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd 
 import datetime
-from demand import prepare_predictors
-from discounts import apply_discount
+import ast 
+from sklearn.linear_model import Ridge
+
+from demand import *
+from discounts import apply_discount, disc_per_day
+
 def create_bb_data(inventory, product):
     """
     For a given product it returns a dataframe containing extensive info about wasted items
@@ -104,22 +108,88 @@ def predicted_demand(df_waste, ranges, model, input_dct, prep_transactions):
     """
     pred_values = []
     opt_values = []
+    avg_salesprice = []
+    i = 0
     for rang in ranges:
-        #TODO: check if there is actual waste and only apply discounts then
+        #TODO: create new column of average price
+        
         df_frange = prep_transactions[(prep_transactions["DOY"] >= rang[0]) & (prep_transactions["DOY"] <= rang[1])]
+        #sum of all sales_price*purchases
+        prices = (df_frange["count"]*df_frange["purchase_price"]).sum()
+        #nr of purchases 
+        purchases = df_frange["count"].sum()
         try:
             frange_prepared = prepare_predictors(df_frange, input_dct)
-            #TODO: add discounts 
-            frange_mod = frange_prepared.copy() #copy of the prepared df for appliying discounts
-            last = len(frange_mod)-1
-            frange_mod.loc[last:last] = apply_discount(frange_mod.loc[last:last],30)
-            frange_mod.loc[last-1:last-1] = apply_discount(frange_mod.loc[last-1:last-1],20)
             est_demand = model.predict(frange_prepared).sum()
-            est_mod_demand = model.predict(frange_mod).sum()
-            pred_values.append(est_demand)
-            opt_values.append(est_mod_demand)
+            pred_values.append(round(est_demand))
+            #TODO: add discounts 
+            if df_waste.iloc[i]["waste"] > 0: #if there is waste
+                frange_mod = frange_prepared.copy() #copy of the prepared df for appliying discounts
+                last = len(frange_mod)-1
+                frange_mod.loc[last-1:last-1] = apply_discount(frange_mod.loc[last-1:last-1],50) #day before goes bad
+                frange_mod.loc[last:last] = apply_discount(frange_mod.loc[last:last],50) #actual day it goes bad
+                
+                est_mod_demand = model.predict(frange_mod).sum()
+                ratio = float(est_mod_demand/est_demand)
+                sales_prices = (frange_mod["purchase_price"].values*(df_frange["count"].values*ratio)).sum()
+                purchases = df_frange["count"].sum()*ratio
+                avg = sales_prices/purchases
+                opt_values.append(round(est_mod_demand))
+                
+                avg_salesprice.append(avg)
+            else:
+                opt_values.append(round(est_demand))
+                avg_salesprice.append(prices/purchases)
+                i += 1
         except(ValueError):
-            pass
+            print("Value error occurred")
+            
+
     df_waste["predicted demand"] = pred_values
     df_waste["demand_discounts"] = opt_values
+    ratio=sum(opt_values)/sum(pred_values)
+    df_waste["avg_price"] = avg_salesprice
+    df_waste["expected purchases"] = round(df_waste["purchases"]*ratio)
+    df_waste["expected waste"] = df_waste["waste"] - (df_waste["expected purchases"]-df_waste["purchases"])
+    df_waste["expected waste"] = df_waste[df_waste["expected waste"] > 0]["expected waste"]
+
     return df_waste
+
+
+def waste_analysis(inventory, transactions, df_product, product:str, model = Ridge(), discount_per_day=None):
+    """
+    Contains every function that is needed to return the analysis in the combininb_waste notebook
+    """
+    #convert string into tuples in the inventory table
+    for colname in inventory.columns[2:]:
+        inventory[colname] = [ast.literal_eval(i) for i in inventory[colname]]
+
+    filtered_transactions = transactions[transactions["description"] == product]
+    filtered_inventory = inventory[["day", "before or after delivery", product]]
+    waste_df = create_bb_data(filtered_inventory, product)
+    waste_df_ext = add_purchases(waste_df, transactions, product)
+    print("Extended waste dataframe created")
+    ranges = get_ranges(inventory, product)
+    if discount_per_day == None:
+        discounts_per_day = disc_per_day(transactions)
+    else:
+        discounts_per_day = discount_per_day.copy()
+    prepared_transactions = prepare_data(filtered_transactions, discounts_per_day=discounts_per_day)
+    print("Data prepared for prediction")
+    output_dct = prepare_demand_function(prepared_transactions)
+    model = fit_demand_function(output_dct, model)
+    print("Demand function fitted")
+    test = predicted_demand(waste_df_ext, ranges, model, output_dct, prepared_transactions)
+    print("Waste change predicted, calculating revenue loss...")
+    purchase_price = df_product[df_product["description"] == product]["purchase_price"].values
+    waste_df_ext.fillna({"expected waste":0}, inplace=True)
+    waste_df_ext["expected loss revenue"] = waste_df_ext["expected waste"]*waste_df_ext["avg_price"]
+    waste_df_ext["expected loss profit"] = waste_df_ext["expected waste"]*(waste_df_ext["avg_price"]-purchase_price)
+    waste_df_ext["expected waste cost"] = waste_df_ext["expected waste"]*purchase_price
+
+    waste_df_ext["loss revenue"] = waste_df_ext["waste nn"]*waste_df_ext["avg_price"]
+    waste_df_ext["loss profit"] = waste_df_ext["waste nn"]*(waste_df_ext["avg_price"]-purchase_price)
+    waste_df_ext["waste cost"] = waste_df_ext["waste nn"]*purchase_price
+
+    return waste_df_ext
+
